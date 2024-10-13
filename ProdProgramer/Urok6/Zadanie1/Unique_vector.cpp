4953bf0e6ff2e5fa738dc9f776d84fa4e19cf2c8
Fedor <rozzarrio_ogro@mail.ru>
2024-10-13 11:35:42 +0300

    Urok6_prod

---
+++ ProdProgramer/Urok6/Zadanie1/Unique_vector.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+
+template <class T>
+std::vector<T> Unique_vector(const std::vector<T>& vec)
+{
+	std::vector<T> vec1{ vec };
+	std::sort(vec1.begin(), vec1.end());
+	auto it = std::unique(vec1.begin(), vec1.end());
+	vec1.erase(it, vec1.end());
+	return vec1;
+}
+
+///////////////////////////////////////
+
+template<class T>
+void printVec(const std::vector<T>& arr)
+{
+	for (auto i : arr) std::cout << i << " ";
+}
+
+////////////////////////////////////////////////////
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	std::vector<int> test{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	printVec(test);
+	std::cout << "\n";
+	printVec(Unique_vector(test));
+
+	return 0;
+}
---
+++ ProdProgramer/Urok6/Zadanie2/Print_container.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <set>
+#include <list>
+
+
+template<class T>
+void print_container(const T& arr)
+{
+	for (auto i : arr) std::cout << i << " ";
+}
+
+////////////////////////////////////////////////////
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set); // four one three two. помните почему такой порядок? :)
+	std::cout << "\n";
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list); // one, two, three, four
+	std::cout << "\n";
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector); // one, two, three, four
+	std::cout << "\n";
+	
+	return 0;
+}

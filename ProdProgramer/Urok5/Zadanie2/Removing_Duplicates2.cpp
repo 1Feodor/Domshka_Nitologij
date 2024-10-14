a8c81e9770200f1e5266340db947612b6f53bc0e
Fedor <rozzarrio_ogro@mail.ru>
2024-10-14 23:15:00 +0300

    Urok6_prod_zamena

---
+++ ProdProgramer/Urok5/Zadanie2/Removing_Duplicates2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	
+	std::vector<int> myvec(std::istream_iterator<int>{std::cin}, std::istream_iterator<int>{});
+
+	std::cout << "\n";
+	std::sort(myvec.begin(), myvec.end(), [](int a, int b) {return a > b; });
+	auto it = std::unique(myvec.begin(), myvec.end());
+	myvec.erase(it, myvec.end());
+	for (auto i : myvec)
+	{
+		std::cout << i << "\n";
+	}
+	return 0;
+}

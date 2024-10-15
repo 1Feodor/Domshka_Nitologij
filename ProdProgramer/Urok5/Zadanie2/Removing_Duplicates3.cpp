a24d6e5c3ca8e2cc7da0d2ebf6adc979d6941935
Fedor <rozzarrio_ogro@mail.ru>
2024-10-15 22:35:49 +0300

    Urok5_prod_zamena

--- ProdProgramer/Urok5/Zadanie2/Removing_Duplicates3.cpp
+++ ProdProgramer/Urok5/Zadanie2/Removing_Duplicates3.cpp
@@ -8,8 +8,20 @@ int main()
 {
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
-	
-	std::vector<int> myvec(std::istream_iterator<int>{std::cin}, std::istream_iterator<int>{});
+
+	std::cout << "Укажите размер массива: ";
+	unsigned int vector_size{ 0 };
+	std::cin >> vector_size;
+
+	std::vector<int> myvec(vector_size);
+	std::cout << "Укажите значения элементов:\n";
+	for (unsigned int i = 0; i < vector_size; i++)
+	{
+		std::cout << "arr[" << i << "] = ";
+		int val = 0;
+		std::cin >> val;
+		myvec.at(i) = val;
+	}
 
 	std::cout << "\n";
 	std::sort(myvec.begin(), myvec.end(), [](int a, int b) {return a > b; });

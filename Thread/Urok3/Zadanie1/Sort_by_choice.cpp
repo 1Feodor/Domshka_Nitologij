31a4690440eeb592b041f224f8211a452f1fb856
Fedor <rozzarrio_ogro@mail.ru>
2024-12-24 17:46:53 +0300

    Urok3_Thread

---
+++ Thread/Urok3/Zadanie1/Sort_by_choice.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <future>
+
+void search_min(const std::vector<int>& list, const std::vector<int>::iterator &it, std::promise<int> prom)
+{
+	int value{ *it };
+	for (auto j = it; j < list.end(); ++j)
+	{
+		if (value > *j)
+			std::swap(value, *j);
+	}
+	prom.set_value(value);
+}
+
+void sort(std::vector<int> &list)
+{	
+	std::future<int> fut;
+	std::future<void> asy;
+	//std::promise<int> prom;
+	//fut = prom.get_future();
+	for (auto i = list.begin(); i < list.end(); ++i)
+	{
+		std::promise<int> prom;
+		fut = prom.get_future();
+		asy = std::async(search_min, std::ref(list), std::ref(i), move(prom));
+		*i = fut.get();
+	}
+}
+
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+	std::vector<int> list{ 5,44,6,85,21,3,48,71,55,6 };
+	for (auto i : list) std::cout << i << " ";
+	std::cout << std::endl;
+
+	sort(list);
+	for (auto i : list) std::cout << i << " ";
+
+	return 0;
+}
---
+++ Thread/Urok3/Zadanie2/For_each.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <mutex>
+#include <vector>
+#include <future>
+
+template < class Iterator, class Predicate>
+void For_each(Iterator begins, Iterator ends, Predicate &foo)
+{
+	int size{ std::distance(begins, ends) };
+	if (size <= 1)
+	{
+		foo(*begins);
+	}
+	else
+	{
+		Iterator mid{ begins };
+		std::advance(mid, size / 2);
+		std::future<void> first_half{ std::async(For_each<Iterator, Predicate>, begins, mid, std::ref(foo)) };
+		std::future<void> second_half{ std::async(For_each<Iterator, Predicate>, mid, ends, std::ref(foo)) };
+	}
+}
+
+std::mutex m;
+
+void prints(int x)
+{
+	std::lock_guard<std::mutex> Lg{ m };
+	std::cout << x << " ";
+}
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+	std::vector<int> list{ 5,44,6,85,21,3,48,71,55,6 };
+	for (auto i : list) std::cout << i << " ";
+	std::cout << std::endl;
+
+	For_each(list.begin(), list.end(), prints);
+	return 0;
+}

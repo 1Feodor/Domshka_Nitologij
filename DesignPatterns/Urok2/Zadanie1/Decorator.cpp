82b45545d5e2a99c8cbd9e048bce423133655aff
Fedor <rozzarrio_ogro@mail.ru>
2025-02-02 23:41:23 +0300

    Design_Patterns_Urok2

---
+++ DesignPatterns/Urok2/Zadanie1/Decorator.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+
+class Text {
+public:
+	virtual void render(const std::string& data) const {
+		std::cout << data;
+	}
+};
+
+class DecoratedText : public Text {
+public:
+	explicit DecoratedText(Text* text) : text_(text) {}
+	~DecoratedText()
+	{
+		//delete text_; // не знаю как правильно освободить память после "new"
+	}
+	Text* text_ = nullptr;
+};
+
+class ItalicText : public DecoratedText {
+public:
+	explicit ItalicText(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data)  const {
+		std::cout << "<i>";
+		text_->render(data);
+		std::cout << "</i>";
+	}
+};
+
+class BoldText : public DecoratedText {
+public:
+	explicit BoldText(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data) const {
+		std::cout << "<b>";
+		text_->render(data);
+		std::cout << "</b>";
+	}
+};
+
+class Paragraph : public DecoratedText {
+public:
+	explicit Paragraph(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data) const {
+		std::cout << "<p>";
+		text_->render(data);
+		std::cout << "</p>";
+	}
+};
+
+class Reversed : public DecoratedText {
+public:
+	explicit Reversed(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data) const {
+		std::string _data_{ data };
+		std::reverse(_data_.begin(), _data_.end());
+		text_->render(_data_);
+	}
+};
+
+class Link : public DecoratedText {
+public:
+	explicit Link(Text* text) : DecoratedText(text) {}
+	void render(const std::string& data, const std::string& data1) const {
+		std::cout << "<a href=";
+		text_->render(data);
+		std::cout << ">";
+
+		text_->render(data1);
+		std::cout << "</a>";
+	}
+};
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+	auto text_block = new ItalicText(new BoldText(new Text()));
+	text_block->render("Hello world");
+	std::cout << std::endl;
+
+	auto text_block1{ new Paragraph(new Text()) };
+	text_block1->render("Hello world");
+	std::cout << std::endl;
+
+	auto text_block2 = new Reversed(new Text());
+	text_block2->render("Hello world");
+	std::cout << std::endl;
+
+	auto text_block3 = new Link(new Text());
+	text_block3->render("netology.ru", "Hello world");
+	std::cout << std::endl;
+
+	//delete text_block; // не знаю как правильно освободить память после "new"
+	return 0;
+}
---
+++ DesignPatterns/Urok2/Zadanie2/VeryHeavyDatabase.cpp
@@ -0,0 +1,107 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+#include <Windows.h>
+
+
+
+class VeryHeavyDatabase 
+{
+public:
+    std::string get_data(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class CacheProxyDB : VeryHeavyDatabase 
+{
+    std::map<std::string, std::string> cache_;
+    std::unique_ptr<VeryHeavyDatabase> real_DB_;
+public:
+    explicit CacheProxyDB(std::unique_ptr<VeryHeavyDatabase> real_object) : real_DB_{ move(real_object) } {}
+    std::string get_data(const std::string& key) noexcept 
+    {
+        if (cache_.find(key) == cache_.end()) 
+        {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_DB_->get_data(key);
+        }
+        else 
+        {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+};
+
+class TestDB : VeryHeavyDatabase {
+    std::unique_ptr<VeryHeavyDatabase> real_DB_;
+public:
+    explicit TestDB(std::unique_ptr<VeryHeavyDatabase> real_object) : real_DB_{ move(real_object) } {}
+    std::string get_data(const std::string& key) noexcept {
+        return "test_data" + key;
+    }
+};
+
+class OneShotDB : VeryHeavyDatabase {
+    std::unique_ptr<VeryHeavyDatabase> real_DB_;
+    size_t shots;
+public:
+    explicit OneShotDB(std::unique_ptr<VeryHeavyDatabase> real_object, size_t shots = 1)
+        : real_DB_{ move(real_object) }, shots{ shots } {}
+    std::string get_data(const std::string& key) noexcept
+    {
+        if (this->shots)
+        {
+            --this->shots;
+            std::cout << "Get from real object\n";
+            return real_DB_->get_data(key);
+        }
+        return "error\n";
+    }
+
+};
+
+std::unique_ptr<CacheProxyDB> init_cache()
+{
+    auto original_DB = std::make_unique<VeryHeavyDatabase>();
+    return std::make_unique<CacheProxyDB>(move(original_DB));
+}
+
+std::unique_ptr<TestDB> init_test()
+{
+    auto original_DB = std::make_unique<VeryHeavyDatabase>();
+    return std::make_unique<TestDB>(move(original_DB));
+}
+
+std::unique_ptr<OneShotDB> init_limit()
+{
+    auto original_DB = std::make_unique<VeryHeavyDatabase>();
+    return std::make_unique<OneShotDB>(move(original_DB), 2);
+}
+
+int main()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+
+    auto DB{ init_cache() };
+    std::cout << DB->get_data("key1") << std::endl;
+    std::cout << DB->get_data("key1") << std::endl;
+    std::cout << DB->get_data("key2") << std::endl;
+    std::cout << std::endl;
+
+    auto DB_test{ init_test() };
+    std::cout << DB_test->get_data("key1") << std::endl;
+    std::cout << DB_test->get_data("key1") << std::endl;
+    std::cout << DB_test->get_data("key2") << std::endl;
+	std::cout << std::endl;
+
+    auto DB_limit{ init_limit() };
+    std::cout << DB_limit->get_data("key1") << std::endl;
+    std::cout << DB_limit->get_data("key1") << std::endl;
+    std::cout << DB_limit->get_data("key2") << std::endl;
+    std::cout << std::endl;
+	
+	return 0;
+}
